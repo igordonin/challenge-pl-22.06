36f798888c94e96fb5d7aa6447bf754a8ebf573c
Igor Donin <igordonin@gmail.com>
2022-06-11 15:19:34 -0300

    feat: implement crud service and endpoints for user domain (#12)
    * chore: add validation pipe to app global pipes
    
    * chore: create dtos for creating and updating a user
    
    * feat: add user crud services and endpoints
    
    * chore: add rest client mock file for manually testing endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user-dto';
+import { UpdateUserDto } from './dtos/update-user-dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) { }
+
+  @Post('/signup')
+  createUser(@Body() userDto: CreateUserDto) {
+    return this.usersService.create(userDto);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() userDto: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), userDto);
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOneById(parseInt(id));
+
+    if (!user) {
+      throw new NotFoundException(`User identified by ${id} not found`);
+    }
+
+    return user;
+  }
+
+  @Get()
+  findUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dtos/create-user-dto';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repository: Repository<User>) { }
+
+  create(userDto: CreateUserDto) {
+    const { email, password } = userDto;
+    const user = this.repository.create({ email, password });
+    return this.repository.save(user);
+  }
+
+  findOneById(id: number) {
+    return this.repository.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.repository.find({ where: { email } });
+  }
+
+  async update(id: number, userAttributes: Partial<User>) {
+    const user = await this.findOneById(id);
+
+    if (!user) {
+      throw new NotFoundException(`User identified by ${id} not found`);
+    }
+
+    Object.assign(user, userAttributes);
+
+    return this.repository.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOneById(id);
+
+    if (!user) {
+      throw new NotFoundException(`User identified by ${id} not found`);
+    }
+
+    // TODO maybe this should be a soft delete
+    this.repository.remove(user);
+  }
+}
