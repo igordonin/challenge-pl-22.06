f4ecf94844db08749ee6e2622b93ff3f5be262bf
igordonin <igordonin@gmail.com>
2022-06-14 14:41:41 -0300

    chore: create decorator to get current-user as controller argument
    - An Interceptor is needed because of DI
    - Create Decorator
    - Create Interceptor
    - Register Global Interceptor
    - Downside: will make a request to the DB even when not required

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,9 +12,11 @@ import {
 } from '@nestjs/common';
 import { Serialize } from 'src/interceptors/serialize-interceptor';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
 import { CreateUserDto } from './dtos/create-user-dto';
 import { UpdateUserDto } from './dtos/update-user-dto';
 import { UserDto } from './dtos/user-dto';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 @Serialize(UserDto)
@@ -24,6 +26,16 @@ export class UsersController {
     private usersService: UsersService,
     private authService: AuthService,
   ) {}
+
+  @Post('/signout')
+  signout(@Session() session: any) {
+    session.userId = null;
+  }
+
+  @Get('/current-user')
+  currentUser(@CurrentUser() user: User) {
+    return user;
+  }
 
   @Post('/signup')
   async signup(@Body() userDto: CreateUserDto, @Session() session: any) {
