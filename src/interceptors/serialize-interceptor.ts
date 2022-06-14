bfc78d0b54c6f28ca0cd2ead16d8fd81c884fd35
igordonin <igordonin@gmail.com>
2022-06-14 09:38:33 -0300

    chore: remove password attribute from user serialization
    Create a reusable customizable Interceptor to serialize responses
    Create a UserDto to only expose desired attributes
    Apply Interceptor on the UserController

---
+++ src/interceptors/serialize-interceptor.ts
@@ -0,0 +1,26 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { map, Observable } from 'rxjs';
+
+export function Serialize(dto: any) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data: any) =>
+        plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        }),
+      ),
+    );
+  }
+}
