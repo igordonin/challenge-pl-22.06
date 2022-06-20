ccb1d29694fdaba6197f94c1e61c33a3f4aabb1d
igordonin <igordonin@gmail.com>
2022-06-20 18:03:29 -0300

    chore: add skaffold descriptors for gcp deployment
    - Add api to the base path of every backend to split backend and frontend routes
    - Create Dockerfiles for both projects
    - Create deployment for MongoDB
    - Create deployment for ingress-ngnix

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,14 @@
+import { RequestMethod } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: 'health', method: RequestMethod.GET }],
+  });
+
   await app.listen(3000);
 }
 bootstrap();
