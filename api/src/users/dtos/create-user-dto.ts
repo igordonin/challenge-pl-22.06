5e722fd99cf72727c235e876b11db94f2143cf1c
igordonin <igordonin@gmail.com>
2022-06-20 18:12:59 -0300

    refactor: minor fixes to have unique email column and validate email

--- api/src/users/dtos/create-user-dto.ts
+++ api/src/users/dtos/create-user-dto.ts
@@ -1,7 +1,7 @@
 import { IsEmail, IsString } from 'class-validator';
 
 export class CreateUserDto {
-  @IsEmail()
+  @IsEmail({}, { message: 'You must provide a valid e-mail' })
   email: string;
 
   @IsString()
