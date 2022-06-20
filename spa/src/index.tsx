8fe9c6bcd90f9d849a5829338b624bd6ea3c8474
igordonin <igordonin@gmail.com>
2022-06-20 11:24:43 -0300

    chore: simpler eslint rules

--- spa/src/index.tsx
+++ spa/src/index.tsx
@@ -11,5 +11,6 @@ const store = createStore(reducers, applyMiddleware(thunk));
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
-, document.querySelector('#root'));
+  </Provider>,
+  document.querySelector('#root')
+);
