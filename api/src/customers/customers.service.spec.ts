f11645cd8e598941c145d7d26971b61475ecc959
Igor Donin <igordonin@gmail.com>
2022-06-21 11:45:01 -0300

    chore: fix api tests (#44)
    * chore: fix api tests
    
    * chore: fix automation test script reference

--- api/src/customers/customers.service.spec.ts
+++ api/src/customers/customers.service.spec.ts
@@ -1,18 +1,72 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Customer } from './customer.entity';
 import { CustomersService } from './customers.service';
+import { CreateCustomerDto } from './dtos/create-customer.dto';
 
 describe('CustomersService', () => {
   let service: CustomersService;
+  let repositoryMock: Partial<Repository<Customer>>;
+
+  const repositoryToken = getRepositoryToken(Customer);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CustomersService],
+      providers: [
+        CustomersService,
+        {
+          provide: repositoryToken,
+          useValue: {
+            create: jest.fn((customerDto: CreateCustomerDto) => {
+              const customer = new Customer();
+              customer.fullName = customerDto.fullName;
+              return customer;
+            }),
+            save: jest.fn((customer: Customer) => customer),
+            find: jest.fn(),
+            findOneBy: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<CustomersService>(CustomersService);
+    repositoryMock = module.get<Repository<Customer>>(repositoryToken);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a customer and save to the db', async () => {
+    const companyDto = {
+      companyName: 'Company Name',
+      companyCountry: 'BR',
+      companyWebsite: 'https://company.site',
+    };
+    const kpisDto = {
+      netPromoterScore: 10,
+      customerSatisfactionScore: 20,
+      customerEffortScore: 30,
+      leadScore: 40,
+    };
+
+    const customerDto = {
+      fullName: 'Full Name',
+      phoneNumber: '12345',
+      email: 'fullname@email.com',
+      jobTitle: 'Job Title',
+      lastContactUtcDate: new Date().toUTCString(),
+      company: companyDto,
+      kpis: kpisDto,
+    };
+
+    const result = await service.create(customerDto);
+
+    expect(repositoryMock.create).toBeCalledTimes(1);
+    expect(repositoryMock.create).toBeCalledWith(customerDto);
+    expect(repositoryMock.save).toBeCalledTimes(1);
+    expect(result.fullName).toEqual(customerDto.fullName);
   });
 });
--- api/src/users/users.service.spec.ts
+++ api/src/users/users.service.spec.ts
@@ -1,15 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let repositoryMock: Partial<Repository<User>>;
+
+  const repositoryToken = getRepositoryToken(User);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: repositoryToken,
+          useValue: {
+            create: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    repositoryMock = module.get<Repository<User>>(repositoryToken);
   });
 
   it('should be defined', () => {
